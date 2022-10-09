ae41643c858a3f8da0b3fb570cf32184168a040d
aniruddha123reinforcement <81187574+aniruddha123reinforcement@users.noreply.github.com>
2022-10-08 20:05:11 -0700

    Update env.py

--- scripts/env.py
+++ scripts/env.py
@@ -50,7 +50,7 @@ class Knowledgegraph_gym(gym.Env):
 		state_idx = [env.entity2id_[sample[0]], env.entity2id_[sample[1]], 0]
         	self.state = preprocessing.idx_state(state_idx)
 		self.steps_beyond_terminated = None
-		return self.state,{}
+		return np.array(self.state,dtype = np.float32),{}
 	
 
 	def step(self, action):
